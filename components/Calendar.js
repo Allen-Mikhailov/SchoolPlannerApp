d0572135de0f32064d85b80d29d4eab8fcb88a3a
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-09-09 23:30:14 -0500

    Added an AddItem popup

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Button, StyleSheet, Text, View, SectionList, SafeAreaView, StatusBar, Dimensions   } from 'react-native';
+import { Button, StyleSheet, Text, View, SectionList, SafeAreaView, StatusBar, Dimensions, TextInput } from 'react-native';
 
 import { getJsonData, storeData } from '../modules/storage';
 
@@ -44,7 +44,34 @@ const styles = StyleSheet.create({
       title: {
         fontSize: 15,
         color: "#2a5996"
-      }
+      },
+
+    transfer: {
+      width: "100%",
+      height: "100%"
+    },
+
+    addItemContainer: {
+      flex: 1,
+      backgroundColor: "#eee",
+      width: "75%",
+      height: "75%",
+      position: "absolute",
+      top: "12.5%",
+      left: "12.5%",
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    dataInput: {
+      fontSize: "25%",
+      textAlign: "center",
+    },
+
+    dateSlashes: {
+      fontSize: "25%",
+      textAlign: "center",
+    }
 })
 
 const Item = ({ title }) => (
@@ -61,9 +88,49 @@ const Header = ({ title }) => (
   </View>
 )
 
+function AddItemMenu(props)
+{
+  const [year, setYear ] = useState("2022")
+  const [day, setDay ] = useState()
+  const [month, setMonth ] = useState()
+  const [date, setDate] = useState(new Date())
+  const [open, setOpen] = useState(true)
+
+  function dayCheck(value)
+  {
+    setDay(parseInt(value)? (Math.min(parseInt(value), 31)).toString(): "")
+    // setDay(toString(15))
+    // setDay("15")
+  }
+
+  function monthCheck(value)
+  {
+    setMonth(parseInt(value)? (Math.min(parseInt(value), 12)).toString(): "")
+  }
+
+  return <View style={styles.addItemContainer}>
+    <TextInput
+    style={styles.dataInput}
+      value={month}
+      onChangeText={monthCheck}
+      placeholder="Month"
+      keyboardType="numeric"
+    />
+    <Text style={styles.dataInput} >/</Text>
+    <TextInput
+    style={styles.dataInput}
+    value={day}
+    onChangeText={dayCheck}
+    placeholder="Day"
+    keyboardType="numeric"
+  />
+  </View>
+}
+
 export default function Calendar()
 {
     const [ data, setData ] = useState([])
+    const [ menuOpen, setMenuOpen ] = useState("None")
 
     useEffect(() => {
         const rawData = [
@@ -75,14 +142,22 @@ export default function Calendar()
         setData(rawData)
     }, [])
 
+    const addToData = () => {
+      setMenuOpen(menuOpen == "AddItem"? "None":"AddItem")
+    }
+
     return (
-        <SafeAreaView style={styles.container}>
-             <SectionList style={{width: "100%"}}
-                sections={data}
-                keyExtractor={(item, index) => item + index}
-                renderItem={({ item }) => <Item title={item} />}
-                renderSectionHeader={({ section: { title } }) => <Header title={title} />}
-            />
+        <SafeAreaView style={styles.transfer}>
+          <View style={styles.container}>
+            <SectionList style={{width: "100%"}}
+                  sections={data}
+                  keyExtractor={(item, index) => item + index}
+                  renderItem={({ item }) => <Item title={item} />}
+                  renderSectionHeader={({ section: { title } }) => <Header title={title} />}
+              />
+          </View>
+          {menuOpen == "AddItem" && <AddItemMenu/>}
+          <Button title={"Add Item"} onPress={addToData}/>
         </SafeAreaView>
     );
 }
